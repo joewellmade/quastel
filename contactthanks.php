ceacb2bd238008eba14cb97a2296bbbb63657b82
Joseph Bramall <joe@wellmadestudio.com>
2016-05-26 09:25:29 +0100

    Added site files

---
+++ contactthanks.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Thank you for getting in touch</title>
+	
+	<link rel="stylesheet" href="stylesheets/app.css" />
+	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/8003292/688706/css/fonts.css" />
+</head>
+
+<body id="about-us">
+
+	<header>
+    
+    <div class="contain-to-grid sticky">
+      <nav class="top-bar" data-topbar role="navigation">
+        <ul class="title-area">
+          <li class="name">
+            <a href="index.html"><img src="img/logo.png" alt="quastel logo" class="logo" /></a>
+          </li>
+           <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+          <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+        </ul>
+
+        <section class="top-bar-section">
+          <!-- Right Nav Section -->
+          <ul class="right">
+            <li><a href="aboutus.html">About Us</a></li>
+            <li class="has-dropdown">
+              <a href="services.html">Services</a>
+              <ul class="dropdown">
+                <li><a href="insurance.html">Insurance Valuations</a></li>
+                <li><a href="probate.html">Probate and Taxation</a></li>
+                <li><a href="family.html">Family Division</a></li>
+                <li><a href="buying.html">Buying and Selling</a></li>
+              </ul>
+            </li>
+            <li><a href="ourfees.html">Our Fees</a></li>
+            <li><a href="partners.html">Partners</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </section>
+      </nav>
+    </div>
+    </header>
+			
+			
+		<br /><br />
+		
+		<div class="row thanks">
+			<div class="large-12 columns content">
+				<h1>Your message has been sent!</h1><br />
+				<p class="back"><a href="index.html">Back to the Quastel webite</a></p>
+			</div>
+		</div>
+
+	<footer class="home">
+      <div class="row">
+        <div class="large-1 medium-1 columns footerLogo">
+          <img src="img/q.png" alt="Quastel Logo" class="qLogo" />
+        </div>
+        <div class="large-3 medium-3 columns">
+          <p class="info"><span class="copper">Email:</span><br /><a href="mailto:info@quastelassociates.com">info@quastelassociates.com</a><br />
+          <span class="copper">Telephone:</span> <br /><a href="tel:+4402072531710">020 7253 1710</a></p>
+        </div>
+        <div class="large-5 medium-4 columns">
+          <p class="info"><span class="copper">Address:</span><br />    
+          Quastel Associates Limited<br />
+          13 Charterhouse Square,<br /> London, EC1M 6AX</p>
+        </div>
+        <div class="large-3 medium-4 columns footerLogo">
+          <a href="https://www.linkedin.com/company/quastel-associates-ltd" target="blank"><img src="img/linkedin.png" alt="linkedin" class="linkedin" /></a>
+          <a href="http://www.sofaa.org/" target="blank"><img src="img/sofaa.png" alt="SOFAA" class="sofaa" /></a>
+        </div>
+      </div>
+    </footer>
+			
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
